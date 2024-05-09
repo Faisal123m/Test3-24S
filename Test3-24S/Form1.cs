f5e97cec681078daa804b0b59e69b512cc445a97
Faisal123m <s44457286@students.tu.edu.sa>
2024-05-09 09:29:45 +0300

    FAisal sultan

--- Test3-24S/Form1.cs
+++ Test3-24S/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Test3_24S
 {
     public partial class Form1 : Form
@@ -5,6 +7,49 @@ namespace Test3_24S
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sum = int.Parse(textBox1.Text);
+            sum = sum + int.Parse(textBox2.Text);
+            textBox2.Text = sum.ToString();
+            double count = 1;
+            count = count + int.Parse(textBox3.Text);
+            textBox3.Text = (count).ToString();
+            Double avrage = sum / count;
+            textBox4.Text = avrage.ToString();
+
+
+
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
         }
     }
 }
